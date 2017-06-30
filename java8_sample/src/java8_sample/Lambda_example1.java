d3279bf812b1370a0f54f6463e8b006a636e5cdb
Marathon-fan <johnprogrammer2005@gmail.com>
2017-06-30 10:04:34 +1000

    Initial checkin

---
+++ java8_sample/src/java8_sample/Lambda_example1.java
@@ -0,0 +1,15 @@
+package java8_sample;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Lambda_example1 {
+	
+	public static void static_run(){
+		List<String> sList = Arrays.asList("String1", "String2", "String3");
+		sList.forEach(curS -> System.out.println(curS + " hello"));
+		sList.forEach(System.out::println);		
+	}
+}
+
+
---
+++ java8_sample/src/java8_sample/script_entrypoint.java
@@ -0,0 +1,12 @@
+package java8_sample;
+
+import java.util.*;
+
+public class script_entrypoint {
+	
+	public static void main(String[] args) throws Exception{
+		//Lambda_example1.static_run();
+		Lambda_example4.static_run();
+	}	
+
+}
