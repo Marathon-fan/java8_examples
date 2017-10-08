4ba376ef5bb0847dc4ffc77eb89ba99e2ef4074a
John <johnprogrammer2005@gmail.com>
2017-10-08 11:55:17 +1100

    add io examples and concurrency examples

--- java8_sample/src/java8_sample/script_entrypoint.java
+++ java8_sample/src/java8_sample/script_entrypoint.java
@@ -1,19 +1,22 @@
 package java8_sample;
 
-import java.util.*;
+import java8_sample.concurrency.BoxOffice;
+import java8_sample.io.BufferReaderExample;
+import java8_sample.io.BufferedWriterExample;
+import java8_sample.io.StreamAndURLExample;
 //slf4j
 
 public class script_entrypoint {
 	
 	public static void main(String[] args) throws Exception{
 		//Lambda_example1.static_run();
-		int[] nums = {1,2,3,4};
-		for  (int i = 0; i < nums.length; i++){
-			System.out.println(nums[i]); //
-		}
-		functionalInterface_example1.static_run();
-		System.out.println("hello, world");
-		Complex.static_run();
+//		int[] nums = {1,2,3,4};
+//		for  (int i = 0; i < nums.length; i++){
+//			System.out.println(nums[i]); //
+//		}
+//		functionalInterface_example1.static_run();
+//		System.out.println("hello, world");
+		BoxOffice.static_run();
 	}	
 
 }
